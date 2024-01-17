23e8c7d323dce76a7564b37d3a35dee1620becd9
soph-cabral <144605103+soph-cabral@users.noreply.github.com>
2024-01-17 17:20:06 +0000

    commit 6

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.run(debug=True)
